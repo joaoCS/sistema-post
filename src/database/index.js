1d76e96f6d971f90d335dbc1e2a573796baa5a35
joao <joaoantoniogba025@gmail.com>
2019-10-19 09:07:49 -0300

    Alterado banco mongo

--- src/database/index.js
+++ src/database/index.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb+srv://deploy:uploaddeploy@cluster0-zpfz0.mongodb.net/test?retryWrites=true&w=majority', {
+mongoose.connect(process.env.MONGO_URL, {
 
  useNewUrlParser: true,
  useFindAndModify: false,
